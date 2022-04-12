4daf5277c95a09d3fffe3d4b90473796a14057d0
Thijs Creemers <thijs.creemers@gmail.com>
2022-04-12 14:38:08 +0200

    Simplified money and added currency namespace

---
+++ src/ixn/currency.clj
@@ -0,0 +1,57 @@
+(ns ixn.currency
+  (:require [clj-http.client :as client]
+            [jsonista.core :as json]))
+
+
+(defonce rates-cache (atom {:date "1900-01-01" :rates []}))
+
+(def Currency
+  [:map
+   [:currency/iso-code {:en "ISO 3 code" :nl "ISO 2 code"}
+    [:re {:error-message {:en "Invalid ISO-3 code." :nl "Ongeldige ISO-3 code."}} #"^[A-Z]{3}$"]]
+   [:currency/symbol {:en "symbol" :nl "symbool"} string?]
+   [:currency/decimals {:en "decimals" :nl "decimalen"} [:and int? [:>= 0] [:<= 5]]]])
+
+
+;; A list of currencies, add if needed.
+(def currencies
+  {:EUR {:currency/iso-code "EUR" :currency/iso-numeric "978" :currency/symbol "€"  :currency/decimals 2 :currency/name "Euro"}
+   :USD {:currency/iso-code "USD" :currency/iso-numeric "840" :currency/symbol "$"  :currency/decimals 2 :currency/name "US Dollar"}
+   :GBP {:currency/iso-code "GBP" :currency/iso-numeric "826" :currency/symbol "£"  :currency/decimals 2 :currency/name "Britush Pound"}
+   :CHF {:currency/iso-code "CHF" :currency/iso-numeric "756" :currency/symbol "Fr" :currency/decimals 2 :currency/name "Swiss franc"}
+   :AUD {:currency/iso-code "AUD" :currency/iso-numeric "036" :currency/symbol "$"  :currency/decimals 2 :currency/name "Australian dollar"}
+   :JPY {:currency/iso-code "JPY" :currency/iso-numeric "392" :currency/symbol "¥"  :currency/decimals 2 :currency/name "Japanese Yen"}
+   :SEK {:currency/iso-code "SEK" :currency/iso-numeric "752" :currency/symbol "kr" :currency/decimals 2 :currency/name "Swedish krona"}
+   :DKK {:currency/iso-code "DKK" :currency/iso-numeric "208" :currency/symbol "kr" :currency/decimals 2 :currency/name "Danish krona"}
+   :NOK {:currency/iso-code "NOK" :currency/iso-numeric "578" :currency/symbol "kr" :currency/decimals 2 :currency/name "Norwegian krona"}
+   :CAD {:currency/iso-code "CAD" :currency/iso-numeric "124" :currency/symbol "$"  :currency/decimals 2 :currency/name "Canadian dollar"}
+   :PLN {:currency/iso-code "PLN" :currency/iso-numeric "985" :currency/symbol "zł",:currency/decimals 2 :currency/name "Polish Złoty"}})
+
+
+(defn date-yesterday []
+  (.format (java.text.SimpleDateFormat. "yyyy-MM-dd") (java.util.Date. ^Long (- (.getTime (java.util.Date.)) (* 24 60 60 1000)))))
+
+(defn- fetch-rates
+  []
+  (if (< (compare (:date @rates-cache) (date-yesterday)) 0)
+     (let [{:keys [reason status :body]} (client/get "https://www.currency-api.com/rates")]
+       (if (= (:reason "OK"))
+         (reset! rates-cache (-> body (json/read-value json/keyword-keys-object-mapper)))
+         {:error {:status status :reason reason}})))
+  (:rates @rates-cache))
+
+
+(defn rate
+  "Base rate is EUR"
+  [currency]
+  (currency (fetch-rates)))
+
+(defn convert-to-euro [currency amount]
+  (let [rate (rate currency)]
+    (/ (bigdec amount) rate)))
+
+;(defn convert [to-currency currency amount]
+;  (let [rate (rate currency)
+;        euros (convert-to-euro currency amount)]
+;    (prn euros)
+;    (/ (bigdec euros) rate)))
--- src/ixn/money.clj
+++ src/ixn/money.clj
@@ -1,75 +1,64 @@
 (ns ixn.money
-  (:require [malli.core :as m]
-            [clojure.string :as str]
-            [clojurewerkz.money.amounts :as cwma]
-            [clojurewerkz.money.currencies :as cwmc]))
+  (:require [clojure.string :as str]
+            [ixn.currency :as ic]
+            [malli.core :as m]))
+
 
 ;; Malli definition
-(def Currency
-  [:map
-   [:currency/code-2 {:en "ISO 2 code" :nl "ISO 2 code"}
-    [:re {:error-message {:en "Invalid ISO-2 code." :nl "Ongeldige ISO-2 code."}} #"^[A-Z]{2}$"]]
-   [:currency/code-3 {:en "ISO 3 code" :nl "ISO 2 code"}
-    [:re {:error-message {:en "Invalid ISO-3 code." :nl "Ongeldige ISO-3 code."}} #"^[A-Z]{3}$"]]
-   [:currency/symbol {:en "symbol" :nl "symbool"} string?]
-   [:currency/decimals {:en "decimals" :nl "decimalen"} [:and int? [:>= 0] [:<= 5]]]])
-
 (def Money
   [:map {:closed true :en "money" :nl "geld"}
-   [:money/currency {:en "currency" :nl "valuta"} Currency]
+   [:money/currency {:en "currency" :nl "valuta"} keyword?]
    [:money/value {:en "value" :nl "waarde"} int?]])
 
-(def Poen org.joda.money.Money)
-
-;; A list of currencies
-(def currencies
-  {:eur {:currency/code-2 "EU" :currency/code-3 "EUR" :currency/symbol "€" :currency/decimals 2}
-   :usd {:currency/code-2 "US" :currency/code-3 "USD" :currency/symbol "$" :currency/decimals 2}})
 
 ;; Constants
-(defonce default-currency (:eur currencies))
+(defonce default-currency :EUR)
 (defonce decimal-separator ",")
 (defonce thousand-separator ".")
 
-(defn round
-  "Round a double to the given precision (number of significant digits)"
-  [precision d]
-  (let [factor (Math/pow 10 precision)]
-    (/ (Math/round ^Double (* d factor)) factor)))
 
-(defn money? [val]
+(defn round
+  [value decimals]
+  "Round a double to the given precision (number of significant digits)"
+  (let [factor (Math/pow 10 decimals)]
+    (/ (Math/round ^Double (* value factor)) factor)))
+
+
+(defn money?
+  [val]
   "Check id val is valid Money data."
   (m/validate Money val))
 
+
 (defn ->money
-  ([currency value]
-   (let [decimals (:currency/decimals currency)]
-     {:money/currency currency
-      :money/value (long (* (round decimals value) (Math/pow 10 decimals)))}))
+  ([currency-code value]
+   (let [currency-rec (currency-code ic/currencies)
+         decimals (:currency/decimals currency-rec)]
+     {:money/currency currency-code
+      :money/value    (long (* (round value decimals) (Math/pow 10 decimals)))}))
   ([value]
    (->money default-currency value)))
-
-(defn- ->money-from-value
-  ([currency value]
-   {:money/currency currency
-     :money/value value})
-  ([value]
-   (->money-from-value default-currency value)))
 
 (defn <-money
   "Return a big decimal value."
   [money-value]
   (let [value (:money/value money-value)
-        decimals (get-in money-value [:money/currency :currency/decimals])]
+        currency-rec ((:money/currency money-value) ic/currencies)
+        decimals (:currency/decimals currency-rec)]
     (bigdec (/ value (Math/pow 10 decimals)))))
+
+(defn <-money-value
+  [money-value]
+  "Return integer value as registered in money data"
+  (:money/value money-value))
+
 
 (defn mformat
   "Format money into string with currency symbol and thousand separation."
   [money-value]
-  (let [symbol (get-in money-value [:money/currency :currency/symbol])
+  (let [{:currency/keys [symbol decimals]} ((:money/currency money-value) ic/currencies)
         value (str (:money/value money-value))
-        decimals (get-in money-value [:money/currency :currency/decimals])
-        dec-part (subs value  (- (count value) decimals))
+        dec-part (subs value (- (count value) decimals))
         int-part (subs value 0 (- (count value) decimals))
         int-partseparated (->> int-part
                                reverse
@@ -80,44 +69,32 @@
     (str symbol " " int-partseparated decimal-separator dec-part)))
 
 
+
+(defn- ->money-from-value
+  "returns a money value"
+  ([currency value]
+   {:money/currency currency
+    :money/value    value})
+  ([value]
+   (->money-from-value default-currency value)))
+
+
+
 (defn add
   "Add"
   [& args]
   (->money-from-value (apply + (map (fn [x] (:money/value x)) args))))
+
 
 (defn subtract
   "Subtract"
   [& args]
   (->money-from-value (apply - (map (fn [x] (:money/value x)) args))))
 
-(defn multiply
-  "multiply"
-  [& args]
-  [(apply * (map (fn [[v _]] v) args))
-   (apply min (map (fn [[_ v]] v) args))])
-
-(defn divide
-  "divide"
-  [& args]
-  [(apply / (map (fn [[v _]] v) args))
-   (apply min (map (fn [[_ v]] v) args))])
 
 (comment
-  ;; clojurewerkz money
-  (cwma/amount-of cwmc/USD 10.5)
-  (cwma/currency-of
-    (cwma/parse "EUR 10.50"))
-  (cwma/minor-of
-    (cwma/parse "EUR 10.50"))
-  (cwma/major-of
-    (cwma/parse "EUR 10.50"))
-
-
-
   ;; some REPL tests
-  (money? (plus (->money 12.3456) (->money 11.33383838)))
-  (money? (minus (->money 12.3456) (->money 11.33383838)))
-  (<-money (multiply (->money 12.3456) (->money 11.33383838) (->money 2.1)))
-  (divide (->money 12.3456) 2)
-  (apply min [1 2 3 4])
-  (->money 12.345))
+  (money?
+    (add (->money 12.3456) (->money 11.33383838)))
+  (money? (subtract (->money 12.3456) (->money 11.33383838)))
+  (->money 12.34))
